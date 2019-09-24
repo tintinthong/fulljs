2651e7b8a24bc9ca96e87c979ba5b3ee9cb12f07
Justin <justinthong93@gmail.com>
2019-09-24 15:07:03 +0800

    added jwt test file

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-let fs = require('fs');
-let jwt = require('jsonwebtoken');
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
 
 // PAYLOAD
 let payload = {
@@ -13,26 +13,29 @@ let payload = {
 let privateKey = fs.readFileSync('jwt', 'utf8');
 let publicKey = fs.readFileSync('jwt.pub', 'utf8');
 
-console.log(privateKey)
-console.log(publicKey)
-
-console.log(privateKey)
 // Base64 encode some secret file
 let secret = fs.readFileSync('./secret', 'utf8');
 let encodeddata = fs.readFileSync('./secretbase64', 'utf8');
 let buff = Buffer.from(encodeddata.toString(), 'base64');
-console.log(buff.toString())
 
 let signOptions = {
-    issuer: 'My Soft Corp',
-    subject: 'some@user.com',
-    audience: 'http://mysoftcorp.com',
-    expiresIn: '12h',
-    algorithm: 'RS256'
+    issuer: 'My Soft Corp', //Software organization who issues the token
+    subject: 'some@user.com', //Intended user of the token
+    audience: 'http://mysoftcorp.com', //Basically identity of the intended recipient of the token
+    expiresIn: '12h', //Expiration time after which the token will be invalid
+    algorithm: 'RS256' //Encryption algorithm to be used to protect the token } 
 }
+let token = jwt.sign(JSON.stringify(payload), privateKey, { algorithm: 'RS256' }) 
 
-//   jwt.sign(JSON.stringify(payload), secret, { algorithm: 'RS256' }, function (err) {
-//     if (err) {
-//       console.log(err)
-//     }
-//   })
+let verifyOptions = {
+    issuer: 'My Soft Corp', //Software organization who issues the token
+    subject: 'some@user.com', //Intended user of the token
+    audience: 'http://mysoftcorp.com', //Basically identity of the intended recipient of the token
+    expiresIn: '12h', //Expiration time after which the token will be invalid
+    algorithm: 'RS256' //Encryption algorithm to be used to protect the token
+};
+console.log(token)
+
+var legit = jwt.verify(token, publicKey, {algorithm: 'RS256'});
+
+console.log(JSON.stringify(legit))
