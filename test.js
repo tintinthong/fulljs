4d1005ce08aabd8fe91924a10c0f489b08cbefe4
Justin <justinthong93@gmail.com>
2019-09-24 16:25:27 +0800

    adding time functionality

--- test.js
+++ test.js
@@ -1,41 +1,63 @@
+// https://github.com/auth0/node-jsonwebtoken
+'use strict'
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
 
-// PAYLOAD
+// payload
 let payload = {
-    data1: "Data 1",
-    data2: "Data 2",
-    data3: "Data 3",
-    data4: "Data 4",
+    data1: "data 1",
+    data2: "data 2",
+    data3: "data 3",
+    data4: "data 4",
 };
 
-// PRIVATE and PUBLIC key
+// private and public key
 let privateKey = fs.readFileSync('jwt', 'utf8');
 let publicKey = fs.readFileSync('jwt.pub', 'utf8');
 
-// Base64 encode some secret file
-let secret = fs.readFileSync('./secret', 'utf8');
-let encodeddata = fs.readFileSync('./secretbase64', 'utf8');
-let buff = Buffer.from(encodeddata.toString(), 'base64');
-
 let signOptions = {
-    issuer: 'My Soft Corp', //Software organization who issues the token
-    subject: 'some@user.com', //Intended user of the token
-    audience: 'http://mysoftcorp.com', //Basically identity of the intended recipient of the token
-    expiresIn: '12h', //Expiration time after which the token will be invalid
-    algorithm: 'RS256' //Encryption algorithm to be used to protect the token } 
-}
-let token = jwt.sign(JSON.stringify(payload), privateKey, { algorithm: 'RS256' }) 
+    issuer: 'my soft corp', //software organization who issues the token
+    subject: 'some@user.com', //intended user of the token
+    audience: 'http://mysoftcorp.com', //basically identity of the intended recipient of the token
+    expiresIn: '36h', //expiration time after which the token will be invalid
+    algorithm: 'RS256' //encryption algorithm to be used to protect the token
+};
+
+let token = jwt.sign(payload, privateKey, signOptions)
+
+// safe storing for token 
 
 let verifyOptions = {
-    issuer: 'My Soft Corp', //Software organization who issues the token
-    subject: 'some@user.com', //Intended user of the token
-    audience: 'http://mysoftcorp.com', //Basically identity of the intended recipient of the token
-    expiresIn: '12h', //Expiration time after which the token will be invalid
-    algorithm: 'RS256' //Encryption algorithm to be used to protect the token
+    issuer: 'my soft corp', //software organization who issues the token
+    subject: 'some@user.com', //intended user of the token
+    audience: 'http://mysoftcorp.com', //basically identity of the intended recipient of the token
+    expiresIn: '36h', //expiration time after which the token will be invalid
+    algorithm: 'RS256' //encryption algorithm to be used to protect the token
 };
-console.log(token)
 
-var legit = jwt.verify(token, publicKey, {algorithm: 'RS256'});
+var legit = jwt.verify(token, publicKey, verifyOptions)
+var d = new Date();
+let {iat,exp}=legit
 
-console.log(JSON.stringify(legit))
+function timestampToDate(timestamp){
+    let date=new Date(timestamp*1000)
+    return `${date.getUTCDate()}/${date.getUTCMonth()}/${date.getUTCFullYear()} ${date.getUTCHours()}:${date.getUTCMinutes()}`
+}
+console.log(timestampToDate(iat))
+console.log(timestampToDate(exp))
+
+
+// const dates = dates_as_int.map(date => new Date(date).getTime())
+
+// function showDates(legit){
+//     return {
+//         iat:,
+//         nbf:,
+//         exp:,
+//     }
+// } 
+
+function invalidateToken(token){
+
+    var older_token = jwt.sign({  iat: Math.floor(Date.now() / 1000) - 30 }, 'shhhhh');
+}
